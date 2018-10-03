6d33e2b10e09fa32b7dd9a088929e14033baddc9
jason1412 <jasontrabajo1@gmail.com>
2018-10-03 17:21:48 +0200

    insert carousel

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log("hola")
+console.log('antes')
+$(document).ready( function(){
+    $('#carouselOffersButton').click(function(){
+        if($('#carouselOffersButton').children('span').hasClass('fa-pause')){
+            $('#carousel-offers').carousel('pause');
+            $('#carouselOffersButton').children('span').removeClass('fa-pause');
+            $('#carouselOffersButton').children('span').addClass('fa-play');
+        }
+        else if($('#carouselOffersButton').children('span').hasClass('fa-play')){
+            $('#carousel-offers').carousel('cycle');
+            $('#carouselOffersButton').children('span').removeClass('fa-play');
+            $('#carouselOffersButton').children('span').addClass('fa-pause');
+        }
+    })
+})
+console.log('despues')
